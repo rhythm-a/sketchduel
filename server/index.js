4d5cada8ce8ad456045f05a93eebce17d5dae22d
Rhythm <atreja918@gmail.com>
2026-05-20 16:11:49 +0530

    Bind server to 0.0.0.0 for Render

--- server/index.js
+++ server/index.js
@@ -170,6 +170,6 @@ io.on('connection', (socket) => {
   });
 });
 
-httpServer.listen(PORT, () => {
+httpServer.listen(PORT, "0.0.0.0", () => {
   console.log(`[server] Socket.IO listening on port ${PORT}`);
 });
